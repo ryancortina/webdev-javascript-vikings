2395674b898b0352037eb30d989e48361c1b993c
Ryan Cortina <22090@columbushs.com>
2019-12-09 08:43:15 -0500

    almost done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,31 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength
+    }
+    reciveDamage(Damage){
+        this.health -= null;
+
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        this.name = name;
+        this.health;
+        this.health;
+        this.strength = strength;
+    }
+}
 
 // Saxon
 class Saxon {}
 
 // War
 class War {}
+
